392f7a1c557b29a1a7baf68ccc97d1d926e3bde4
sanjoyk <sanjoyksardar01@gmail.com>
2020-01-06 08:35:21 +0530

    make initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const element = <h1 title="foo">Hello</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = React.createElement("h1", {title:"foo"}, "Hello");
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello"
+  }
+}
+
+
+const container = document.getElementById("root");
+// ReactDOM.render(element, container);
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+
+container.appendChild(node);
+
